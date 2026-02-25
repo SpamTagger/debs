5c20754b2b165f20c17ba88548bfef1c7f598c23
John Mertz <git@john.me.tz>
2026-02-25 13:34:05 -0700

    Fix HTML generation bugs
    Add compliant headers and a basic CSS sheet (stolen from the main
    website).

--- dashboard.sh
+++ dashboard.sh
@@ -6,36 +6,52 @@ function recursive_directory() {
   local path=$1
   echo "Starting $path"
   last=$(echo $path | sed -r 's/(.*)\/[^\/]*/\1/')
+  cat >${path}.html <<EOF
+$HTML_HEAD
+<h1>Index of $path</h1>
+EOF
   [[ ! -d $last ]] && mkdir -p $last
   if grep -qv "/" <<<$(echo $path); then
-    echo "Adding parent: <ul><li><a href='/browse.html'>..</a><li>"
+    echo "Adding parent: <ul><li><a href='/browse.html'>..</a><li> >> ${path}.html"
     echo "<ul><li><a href='/browse.html'>..</a><li>" >> ${path}.html
   else
-    echo "Adding parent: <ul><li><a href='/${last}.html'>..</a><li>"
+    echo "Adding parent: <ul><li><a href='/${last}.html'>..</a><li> >> ${path}.html"
     echo "<ul><li><a href='/${last}.html'>..</a><li>" >> ${path}.html
   fi
-  echo "<h1>Index of $path</h1>" > ${path}.html
   for item in $(find $path -mindepth 1 -maxdepth 1 -type d); do
     next=$(echo $item | sed -r 's/.*\/([^\/]*)/\1/')
     [[ "$next" == "." ]] || [[ "$next" == ".." ]] || [[ "$next" =~ ".html$" ]] && continue
-    echo "Adding directory: <li><a href='/${item}.html'>$next/</a></li>"
+    echo "Adding directory: <li><a href='/${item}.html'>$next/</a></li> >> ${path}.html"
     echo "<li><a href='/${item}.html'>$next/</a></li>" >> ${item}.html
     recursive_directory $item
   done
   for pkg in $(find $path -mindepth 1 -maxdepth 1 -type f); do
     [[ "$pkg" =~ .html$ ]] && continue
     file=$(echo $pkg | sed -r 's/.*\/([^\/]*)/\1/')
-    echo "Adding file: <li><a href='/$pkg'>$file</a></li>"
+    echo "Adding file: <li><a href='/$pkg'>$file</a></li> >> ${path}.html"
     echo "<li><a href='/$pkg'>$file</a></li>" >> ${path}.html
   done
-  echo "</ul>" >> ${path}.html
+  echo "</ul></body>" >> ${path}.html
   echo "Ending $path"
 }
 
 # Landing page with all stable packages
 # TODO: Format into table for different releases/architectures
 
-echo "<h1>SpamTagger APT Repo Packages</h1>" > index.html
+HTML_HEAD='
+<html>
+  <head>
+    <title>SpamTagger APT Repository</title>
+    <meta charset="utf-8">
+    <link href="/style.css" rel="stylesheet">
+    <link href="/spamtagger-debs.svg" rel="image/svg+xml">
+  </head>
+  <body>
+'
+cat >index.html <<EOF
+$HTML_HEAD
+<h1>SpamTagger APT Repo Packages</h1>
+EOF
 
 echo "<table><tr><th>Package</th><th>Debian Release</th><th>Version</th><th>Architecture</th><th>Download</th><th>Checksum</th></tr>" >> index.html
 for deb in pool/main/*.deb; do
@@ -45,12 +61,12 @@ for deb in pool/main/*.deb; do
   arch=$(echo $deb | sed -r 's/.*_([^\.]*).deb/\1/')
   echo "<tr><td>$name</td><td>$rel</td><td>$ver</td><td>$arch</td><td><a href='https://debs.spamtagger.org/$deb'>download</a></td><td><a href='https://debs.spamtagger.org/$deb.sha256'>sha256</a></td></tr>" >> index.html
 done
-echo "</tr></table><p><a href='/browse.html'>Browse</a> full APT repository for all package versions and snapshots.</p>" >> index.html
+echo "</tr></table><p><a href='/browse.html'>Browse</a> full APT repository for all package versions and snapshots.</p></body>" >> index.html
 
 echo "<h1>SpamTagger APT Repo Index</h1>" > browse.html
 echo "<ul><li><a href='/index.html'>..</a><li>" >> browse.html
 for i in dists pool snapshots; do
-  echo "<ul><li><a href='/$i.html'>$i/</a><li>" >> browse.html
+  echo "<li><a href='/$i.html'>$i/</a><li>" >> browse.html
   recursive_directory $i
 done
-echo "</ul>" >> browse.html
+echo "</ul></body>" >> browse.html
